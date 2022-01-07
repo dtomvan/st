a07d5190602d9a25316a0d2641b97636c51bb4d6
Tom van Dijk <18gatenmaker6@gmail.com>
2022-01-07 15:52:42 +0100

    Temp commit

--- config.def.h
+++ config.def.h
@@ -97,7 +97,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.8, alphaUnfocused = 0.6;
+float alpha = 0.98, alphaUnfocused = 0.95;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -130,6 +130,8 @@ static const char *colorname[] = {
 	"#C8C093",
 	"#2D4F67",
 	"#1F1F28",
+	// Unfocused BG
+	"#1B1B25",
 };
 
 
@@ -149,7 +151,7 @@ unsigned int selectionfg = 256;
 /* Else if 1 keep original foreground-color of each cell => more colors :) */
 static int ignoreselfg = 1;
 /* NOTE: https://st.suckless.org/patches/alpha_focus_highlight */
-unsigned int bg = 17, bgUnfocused = 16;
+unsigned int bg = 258, bgUnfocused = 259;
 
 /*
  * Default shape of cursor
